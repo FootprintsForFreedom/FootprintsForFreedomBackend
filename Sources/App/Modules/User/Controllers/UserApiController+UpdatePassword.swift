a9e4b0a2ad929a22ba74ef0c8919dfc52efeb1c8
niklhut <49069026+niklhut@users.noreply.github.com>
2022-02-03 21:36:07 +0100

    Moved update password to separate file

---
+++ Sources/App/Modules/User/Controllers/UserApiController+UpdatePassword.swift
@@ -0,0 +1,49 @@
+//
+//  UserApiController+UpdatePassword.swift
+//  
+//
+//  Created by niklhut on 03.02.22.
+//
+
+import Vapor
+
+extension UserApiController: ApiUpdatePasswordController {
+    typealias UpdatePasswordObject = User.Account.ChangePassword
+    
+    @AsyncValidatorBuilder
+    func updatePasswordValidators() -> [AsyncValidator] {
+        KeyedContentValidator<String>.required("currentPassword")
+        KeyedContentValidator<String>.required("newPassword")
+    }
+    
+    /// Require user to be logged in
+    func updatePasswordInput(_ req: Request, _ model: UserAccountModel, _ input: User.Account.ChangePassword) async throws {
+        guard let user = req.auth.get(AuthenticatedUser.self) else {
+            throw Abort(.unauthorized)
+        }
+        
+        /// Assure the user itself changes the password
+        guard model.id == user.id else {
+            throw Abort(.forbidden)
+        }
+        
+        /// Verify current password
+        guard try Bcrypt.verify(input.currentPassword, created: model.password) else {
+            throw Abort(.forbidden)
+        }
+        
+        /// Update the password
+        model.password = try Bcrypt.hash(input.newPassword)
+    }
+    
+    func updatePasswordResponse(_ req: Request, _ model: UserAccountModel) async throws -> Response {
+        try await detailOutput(req, model).encodeResponse(for: req)
+    }
+    
+    func setupUpdatePasswordRoutes(_ routes: RoutesBuilder) {
+        let protectedRoutes = routes.grouped(AuthenticatedUser.guardMiddleware())
+        let baseRoutes = getBaseRoutes(protectedRoutes)
+        let existingModelRoutes = baseRoutes.grouped(ApiModel.pathIdComponent).grouped("updatePassword")
+        existingModelRoutes.put(use: updatePasswordApi)
+    }
+}
