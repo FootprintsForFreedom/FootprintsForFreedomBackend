d3a6e025aa613ddfdc04cfc921e398a9da3b737c
niklhut <49069026+niklhut@users.noreply.github.com>
2022-02-06 22:31:15 +0100

    Set valid token interval to one day

--- Sources/App/Modules/User/Controllers/UserApiController+EmailVerification.swift
+++ Sources/App/Modules/User/Controllers/UserApiController+EmailVerification.swift
@@ -49,7 +49,7 @@ extension UserApiController: ApiEmailVerificationController {
             throw Abort(.unauthorized)
         }
         /// confirm the token is not older than 24 hours
-        guard let createdAt = verificationToken.createdAt, abs(createdAt.timeIntervalSinceNow) < 60 * 60 * 24 else {
+        guard let createdAt = verificationToken.createdAt, abs(createdAt.timeIntervalSinceNow) < 60 * 60 * 60 * 24 else {
             throw Abort(.unauthorized)
         }
         /// verify the token in the request equals the token saved for that user
