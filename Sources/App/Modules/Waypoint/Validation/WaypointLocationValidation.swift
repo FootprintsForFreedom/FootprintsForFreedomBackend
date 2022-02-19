e90c2b863b2172c56180649952f0f7b8c2010f80
niklhut <49069026+niklhut@users.noreply.github.com>
2022-02-19 14:27:26 +0100

    Added location validator

---
+++ Sources/App/Modules/Waypoint/Validation/WaypointLocationValidation.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointLocationValidation.swift
+//  
+//
+//  Created by niklhut on 19.02.22.
+//
+
+extension KeyedContentValidator where T == Waypoint.Location {
+    static func location(_ key: String, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
+        .init(key, "\(key.capitalized) is required", optional: optional) { value, request in
+            value.latitude >= -90 &&
+            value.latitude <= 90 &&
+            value.longitude >= -180 &&
+            value.longitude <= 180
+        }
+    }
+}
