1e2f0c38991ebdc00583e868d4f95c019a01fc96
niklhut <49069026+niklhut@users.noreply.github.com>
2022-02-20 22:15:34 +0100

    Changed fields to editable objects

--- Sources/App/Modules/Waypoint/Database/Models/WaypointMediaModel.swift
+++ Sources/App/Modules/Waypoint/Database/Models/WaypointMediaModel.swift
@@ -27,9 +27,9 @@ final class WaypointMediaModel: DatabaseModelInterface {
     @ID() var id: UUID?
     @Field(key: FieldKeys.v1.verified) var verified: Bool
     
-    @Parent(key: FieldKeys.v1.titleId) var title: EditableObjectRepositoryModel<String>
-    @Parent(key: FieldKeys.v1.descriptionId) var description: EditableObjectRepositoryModel<String>
-    @Parent(key: FieldKeys.v1.sourceId) var source: EditableObjectRepositoryModel<String>
+    @Parent(key: FieldKeys.v1.titleId) var title: EditableObjectModel<String>
+    @Parent(key: FieldKeys.v1.descriptionId) var description: EditableObjectModel<String>
+    @Parent(key: FieldKeys.v1.sourceId) var source: EditableObjectModel<String>
     
     @Enum(key: FieldKeys.v1.group) var group: Waypoint.Media.Group
     
