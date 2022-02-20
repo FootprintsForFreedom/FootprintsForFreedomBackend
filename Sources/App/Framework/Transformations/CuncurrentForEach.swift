1933feadb732a586859dde580b7a18aeb7eae59e
niklhut <49069026+niklhut@users.noreply.github.com>
2022-02-20 22:10:49 +0100

    Added async and concurrent map versions

---
+++ Sources/App/Framework/Transformations/CuncurrentForEach.swift
@@ -0,0 +1,25 @@
+//
+//  CuncurrentForEach.swift
+//  
+//
+//  Created by niklhut on 19.02.22.
+//
+
+import Foundation
+
+extension Sequence {
+    func concurrentForEach(
+        _ operation: @escaping (Element) async -> Void
+    ) async {
+        // A task group automatically waits for all of its
+        // sub-tasks to complete, while also performing those
+        // tasks in parallel:
+        await withTaskGroup(of: Void.self) { group in
+            for element in self {
+                group.addTask {
+                    await operation(element)
+                }
+            }
+        }
+    }
+}
