38852af9463d4ddc9969c4dc1a4324ad5042b431
niklhut <49069026+niklhut@users.noreply.github.com>
2022-03-07 21:26:39 +0100

    Improved language api

--- Sources/App/Modules/Language/Controllers/LanguageApiController.swift
+++ Sources/App/Modules/Language/Controllers/LanguageApiController.swift
@@ -15,6 +15,29 @@ struct LanguageApiController: ApiController {
     typealias ApiModel = Language.Language
     typealias DatabaseModel = LanguageModel
     
+    func onlyForAdmin(_ req: Request) async throws {
+        /// Require user to be signed in
+        let authenticatedUser = try req.auth.require(AuthenticatedUser.self)
+        /// find the user model belonging to the authenticated user
+        guard let user = try await UserAccountModel.find(authenticatedUser.id, on: req.db) else {
+            throw Abort(.unauthorized)
+        }
+        /// require  the user to be a admin or higher
+        guard user.role >= .admin else {
+            throw Abort(.forbidden)
+        }
+    }
+    
+    func requireUniqueLanguageCode(_ req: Request, _ model: LanguageModel) async throws {
+        guard try await LanguageModel
+                .query(on: req.db)
+                .filter(\.$languageCode == model.languageCode)
+                .count() == 0
+        else {
+            throw Abort(.badRequest)
+        }
+    }
+    
     func getBaseRoutes(_ routes: RoutesBuilder) -> RoutesBuilder {
         routes.grouped("languages")
     }
@@ -26,14 +49,25 @@ struct LanguageApiController: ApiController {
         KeyedContentValidator<Bool>.required("isRTL", optional: optional)
     }
     
+    func list(_ req: Request) async throws -> Page<LanguageModel> {
+        let queryBuilder = LanguageModel.query(on: req.db)
+        let numberOfLanguages = try await queryBuilder.count()
+        let list = try await beforeList(req, queryBuilder).paginate(PageRequest(page: 1, per: numberOfLanguages))
+        return try await afterList(req, list)
+    }
+    
+    func beforeList(_ req: Request, _ queryBuilder: QueryBuilder<LanguageModel>) async throws -> QueryBuilder<LanguageModel> {
+        queryBuilder.sort(\.$priority, .ascending) // Lowest value first
+    }
+    
     func listOutput(_ req: Request, _ models: Page<LanguageModel>) async throws -> Page<Language.Language.List> {
         models.map { model in
-                .init(
-                    id: model.id!,
-                    languageCode: model.languageCode,
-                    name: model.name,
-                    isRTL: model.isRTL
-                )
+            return .init(
+                id: model.id!,
+                languageCode: model.languageCode,
+                name: model.name,
+                isRTL: model.isRTL
+            )
         }
     }
     
@@ -68,6 +102,11 @@ struct LanguageApiController: ApiController {
         )
     }
     
+    func beforeCreate(_ req: Request, _ model: LanguageModel) async throws {
+        try await onlyForAdmin(req)
+        try await requireUniqueLanguageCode(req, model)
+    }
+    
     func createInput(_ req: Request, _ model: LanguageModel, _ input: Language.Language.Create) async throws {
         let currentHighestPriority = try await LanguageModel
             .query(on: req.db)
@@ -80,17 +119,35 @@ struct LanguageApiController: ApiController {
         model.priority = currentHighestPriority + 1
     }
     
+    func beforeUpdate(_ req: Request, _ model: LanguageModel) async throws {
+        try await onlyForAdmin(req)
+        try await requireUniqueLanguageCode(req, model)
+    }
+    
     func updateInput(_ req: Request, _ model: LanguageModel, _ input: Language.Language.Update) async throws {
         model.languageCode = input.languageCode
         model.name = input.name
         model.isRTL = input.isRTL
     }
     
+    func beforePatch(_ req: Request, _ model: LanguageModel) async throws {
+        try await onlyForAdmin(req)
+        if try await LanguageModel.find(model.requireID(), on: req.db)?.languageCode != model.languageCode {
+            try await requireUniqueLanguageCode(req, model)
+        }
+    }
+    
     func patchInput(_ req: Request, _ model: LanguageModel, _ input: Language.Language.Patch) async throws {
+        if input.languageCode == nil && input.name == nil && input.isRTL == nil {
+            throw Abort(.badRequest)
+        }
+        
         model.languageCode = input.languageCode ?? model.languageCode
         model.name = input.name ?? model.name
         model.isRTL = input.isRTL ?? model.isRTL
     }
+    
+    // TODO: maybe archive or deactivate language --> just remove the priority; then it does not get indexed anymore
     
     // TODO: reorder language priorities
     
