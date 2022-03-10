63b5c00548f7857dcac64f63979ad42fb97acd5e
niklhut <49069026+niklhut@users.noreply.github.com>
2022-03-10 17:30:50 +0100

    Improved content validator

--- Sources/App/Framework/Validation/KeyedContentValidator+Validations.swift
+++ Sources/App/Framework/Validation/KeyedContentValidator+Validations.swift
@@ -8,11 +8,10 @@
 import Vapor
 
 public extension KeyedContentValidator where T == String {
-
     static func required(_ key: String, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is required", optional: optional) { value, _ in !value.isEmpty }
     }
-        
+    
     static func min(_ key: String, _ length: Int, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is too short (min: \(length) characters)", optional: optional) { value, _ in value.count >= length }
     }
@@ -20,13 +19,13 @@ public extension KeyedContentValidator where T == String {
     static func max(_ key: String, _ length: Int, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is too long (max: \(length) characters)", optional: optional) { value, _ in value.count <= length }
     }
-
+    
     static func alphanumeric(_ key: String, _ message: String? = nil, _ optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) should be only alphanumeric characters", optional: optional) { value, _ in
             !Validator.characterSet(.alphanumerics).validate(value).isFailure
         }
     }
-
+    
     static func email(_ key: String, _ message: String? = nil, _ optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) should be a valid email address", optional: optional) { value, _ in
             !Validator.email.validate(value).isFailure
@@ -35,7 +34,6 @@ public extension KeyedContentValidator where T == String {
 }
 
 public extension KeyedContentValidator where T == Int {
-
     static func min(_ key: String, _ length: Int, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is too short (min: \(length) characters)", optional: optional) { value, _ in value >= length }
     }
@@ -43,21 +41,13 @@ public extension KeyedContentValidator where T == Int {
     static func max(_ key: String, _ length: Int, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is too long (max: \(length) characters)", optional: optional) { value, _ in value <= length }
     }
-
+    
     static func contains(_ key: String, _ values: [Int], _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is an invalid value", optional: optional) { value, _ in values.contains(value) }
     }
 }
 
-public extension KeyedContentValidator where T == UUID {
-
-    static func required(_ key: String, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
-        .init(key, message ?? "\(key.capitalized) is required", optional: optional) { _, _ in true }
-    }
-}
-
-public extension KeyedContentValidator where T == Bool {
-    
+public extension KeyedContentValidator {
     static func required(_ key: String, _ message: String? = nil, optional: Bool = false) -> KeyedContentValidator<T> {
         .init(key, message ?? "\(key.capitalized) is required", optional: optional) { _, _ in true }
     }
