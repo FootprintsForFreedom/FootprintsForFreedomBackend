48bf1548bb68b6b0656fe5777134fc05c051d8e9
niklhut <49069026+niklhut@users.noreply.github.com>
2022-05-10 16:11:47 +0200

    Added file extensions to media groups

--- Sources/AppApi/Modules/Media/MediaGroup.swift
+++ Sources/AppApi/Modules/Media/MediaGroup.swift
@@ -12,5 +12,34 @@ public extension Media.Media {
         public typealias Module = Waypoint
         
         case video, audio, image, document
+        
+        public var allowedFileExtensions: [String] {
+            // file extensions for the groups according to their mime types
+            // see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+            // and https://developer.mozilla.org/en-US/docs/Web/Media/Formats
+            switch self {
+            case .video:
+                // video/quicktime, video/mpeg, video/mp4
+                return ["mov", "mpg", "mpeg", "mpe", "m75", "m15", "mp4", "mpg4"]
+            case .audio:
+                // audio/mpeg, audio/wav
+                return ["mp3", "mpga", "wav", "wave", "bwf"]
+            case .image:
+                // image/png, image/jpeg
+                return ["png", "jpg", "jpeg", "jpe"]
+            case .document:
+                // application/pdf
+                return ["pdf"]
+            }
+        }
+        
+        public static func `for`(_ fileExtension: String) -> Self? {
+            for group in Self.allCases {
+                if group.allowedFileExtensions.contains(fileExtension) {
+                    return group
+                }
+            }
+            return nil
+        }
     }
 }
