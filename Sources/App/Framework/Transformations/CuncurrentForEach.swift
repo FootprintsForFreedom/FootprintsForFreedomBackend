6277a85da27eae852689f7640239c761619cd7ac
niklhut <49069026+niklhut@users.noreply.github.com>
2022-05-15 20:57:04 +0200

    Converted to throwing concurrent for each

--- Sources/App/Framework/Transformations/CuncurrentForEach.swift
+++ Sources/App/Framework/Transformations/CuncurrentForEach.swift
@@ -9,15 +9,15 @@ import Foundation
 
 extension Sequence {
     func concurrentForEach(
-        _ operation: @escaping (Element) async -> Void
+        _ operation: @escaping (Element) async throws -> Void
     ) async {
         // A task group automatically waits for all of its
         // sub-tasks to complete, while also performing those
         // tasks in parallel:
-        await withTaskGroup(of: Void.self) { group in
+        await withThrowingTaskGroup(of: Void.self) { group in
             for element in self {
                 group.addTask {
-                    await operation(element)
+                    try await operation(element)
                 }
             }
         }
