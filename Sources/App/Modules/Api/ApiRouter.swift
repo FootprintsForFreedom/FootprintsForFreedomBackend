6e32215af56800d416c5be09e757b5b06bc7458f
niklhut <49069026+niklhut@users.noreply.github.com>
2022-07-01 11:25:48 +0200

    Removed unused code

--- Sources/App/Modules/Api/ApiRouter.swift
+++ Sources/App/Modules/Api/ApiRouter.swift
@@ -10,14 +10,11 @@ import Vapor
 struct ApiRouter: RouteCollection {
     
     func boot(routes: RoutesBuilder) throws {}
-
+    
     func setUpRoutesHooks(app: Application) throws {
         let apiRoutes = app.routes
-//            .grouped(AuthenticatedUser.guardMiddleware())
             .grouped("api")
-
+        
         let _: [Void] = app.invokeAll("api-routes", args: ["routes": apiRoutes])
     }
-    
-    
 }
