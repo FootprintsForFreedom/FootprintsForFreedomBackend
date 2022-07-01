c625ee5340cb94575ddf0110d3f921d6c3adc0d9
niklhut <49069026+niklhut@users.noreply.github.com>
2022-07-01 13:37:56 +0200

    Removed unused code

--- Sources/App/Modules/User/UserRouter.swift
+++ Sources/App/Modules/User/UserRouter.swift
@@ -8,18 +8,9 @@
 import Vapor
 
 struct UserRouter: RouteCollection {
-    
-//    let frontendController = UserFrontendController()
     let apiController = UserApiController()
     
     func boot(routes: RoutesBuilder) throws {
-//        routes.get("sign-in", use: frontendController.signInView)
-//        routes
-//            .grouped(UserCredentialsAuthenticator())
-//            .post("sign-in", use: frontendController.signInAction)
-//
-//        routes.get("sign-out", use: frontendController.signOut)
-        
         routes.grouped("api")
             .grouped(UserCredentialsAuthenticator())
             .post("sign-in", use: apiController.signInApi)
