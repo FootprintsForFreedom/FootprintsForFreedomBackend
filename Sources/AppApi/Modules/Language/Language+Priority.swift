635f0aac471c61a6c42cb061035ba328692ebe43
niklhut <49069026+niklhut@users.noreply.github.com>
2022-07-01 11:32:36 +0200

    Removed unused code

--- Sources/AppApi/Modules/Language/Language+Priority.swift
+++ Sources/AppApi/Modules/Language/Language+Priority.swift
@@ -8,22 +8,6 @@
 import Foundation
 
 public extension Language.Language {
-//    struct PriorityDetail: Codable {
-//        public let id: UUID
-//        public let languageCode: String
-//        public let name: String
-//        public let isRTL: Bool
-//        public let priority: Int
-//        
-//        public init(id: UUID, languageCode: String, name: String, isRTL: Bool, priority: Int) {
-//            self.id = id
-//            self.languageCode = languageCode
-//            self.name = name
-//            self.isRTL = isRTL
-//            self.priority = priority
-//        }
-//    }
-    
     struct UpdatePriorities: Codable {
         public let newLanguagesOrder: [UUID]
         
