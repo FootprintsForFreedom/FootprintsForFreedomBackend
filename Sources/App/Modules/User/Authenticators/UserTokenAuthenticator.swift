e3f65330918b5e2a91acd1ce06dafc085fc271a3
niklhut <49069026+niklhut@users.noreply.github.com>
2022-07-01 11:44:55 +0200

    Improved formatting

--- Sources/App/Modules/User/Authenticators/UserTokenAuthenticator.swift
+++ Sources/App/Modules/User/Authenticators/UserTokenAuthenticator.swift
@@ -9,12 +9,11 @@ import Vapor
 import Fluent
 
 struct UserTokenAuthenticator: AsyncBearerAuthenticator {
-
     func authenticate(bearer: BearerAuthorization, for req: Request) async throws {
         guard let token = try await UserTokenModel.query(on: req.db).filter(\.$value == bearer.token).first() else {
             return
         }
-
+        
         guard let user = try await UserAccountModel.find(token.$user.id, on: req.db) else {
             return
         }
