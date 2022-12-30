fd0291be0303c1798822c25fb048677a1275400a
niklhut <49069026+niklhut@users.noreply.github.com>
2022-12-30 12:57:12 +0100

    Update Swift Tools Version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "Backend",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     products: [
         .library(name: "AppApi", targets: ["AppApi"]),
