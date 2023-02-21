a0a3d75b2ab63a1749fbb7bb80e15b4c28182b61
niklhut <49069026+niklhut@users.noreply.github.com>
2023-02-21 17:22:52 +0100

    Improvements

--- Sources/App/Modules/Waypoint/Database/Models/WaypointSummaryModel.swift
+++ Sources/App/Modules/Waypoint/Database/Models/WaypointSummaryModel.swift
@@ -290,60 +290,60 @@ extension WaypointSummaryModel.Elasticsearch {
     }
     
     func getTagList(preferredLanguageCode: String?, on elastic: ElasticHandler) async throws -> [Tag.Detail.List] {
-        if !self.tags.isEmpty {
-            var query: [String: Any] = [
-                "query": [
-                    "terms": [
-                        "id": self.tags.map(\.uuidString)
-                    ]
-                ],
-                "collapse": [
-                    "field": "id"
-                ],
-            ]
-            var sort: [[String: Any]] = []
-            if let preferredLanguageCode = preferredLanguageCode {
-                sort.append(
-                    [
-                        "_script": [
-                            "type": "number",
-                            "script": [
-                                "lang": "painless",
-                                "source": "doc['languageCode'].value == params.preferredLanguageCode ? 0 : doc['languagePriority'].value",
-                                "params": [
-                                    "preferredLanguageCode": "\(preferredLanguageCode)"
-                                ]
-                            ],
-                            "order": "asc"
-                        ]
-                    ]
-                )
-            } else {
-                sort.append(["languagePriority": "asc"])
-            }
-            sort.append([ "title.keyword": "asc" ])
-            query["sort"] = sort
-            
-            return try await elastic.perform {
-                guard
-                    let queryData = try? JSONSerialization.data(withJSONObject: query),
-                    let responseData = try? await elastic.custom("/\(LatestVerifiedTagModel.Elasticsearch.wildcardSchema)/_search", method: .GET, body: queryData),
-                    let response = try? ElasticHandler.newJSONDecoder().decode(ESGetMultipleDocumentsResponse<LatestVerifiedTagModel.Elasticsearch>.self, from: responseData)
-                else {
-                    throw Abort(.internalServerError)
-                }
-                
-                return response.hits.hits.map {
-                    let source = $0.source
-                    return .init(
-                        id: source.id,
-                        title: source.title,
-                        slug: source.slug
-                    )
-                }
-            }
-        } else {
+        guard !self.tags.isEmpty else {
             return []
+        }
+        
+        var query: [String: Any] = [
+            "query": [
+                "terms": [
+                    "id": self.tags.map(\.uuidString)
+                ]
+            ],
+            "collapse": [
+                "field": "id"
+            ],
+        ]
+        var sort: [[String: Any]] = []
+        if let preferredLanguageCode = preferredLanguageCode {
+            sort.append(
+                [
+                    "_script": [
+                        "type": "number",
+                        "script": [
+                            "lang": "painless",
+                            "source": "doc['languageCode'].value == params.preferredLanguageCode ? 0 : doc['languagePriority'].value",
+                            "params": [
+                                "preferredLanguageCode": "\(preferredLanguageCode)"
+                            ]
+                        ],
+                        "order": "asc"
+                    ]
+                ]
+            )
+        } else {
+            sort.append(["languagePriority": "asc"])
+        }
+        sort.append([ "title.keyword": "asc" ])
+        query["sort"] = sort
+        
+        return try await elastic.perform {
+            guard
+                let queryData = try? JSONSerialization.data(withJSONObject: query),
+                let responseData = try? await elastic.custom("/\(LatestVerifiedTagModel.Elasticsearch.wildcardSchema)/_search", method: .GET, body: queryData),
+                let response = try? ElasticHandler.newJSONDecoder().decode(ESGetMultipleDocumentsResponse<LatestVerifiedTagModel.Elasticsearch>.self, from: responseData)
+            else {
+                throw Abort(.internalServerError)
+            }
+            
+            return response.hits.hits.map {
+                let source = $0.source
+                return .init(
+                    id: source.id,
+                    title: source.title,
+                    slug: source.slug
+                )
+            }
         }
     }
 }
